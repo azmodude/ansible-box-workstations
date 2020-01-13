28c1431202da8b583e19bb86d57249f105dd0517
Gordon Schulz <gordon.schulz@gmail.com>
2020-01-13 10:46:33 +0100

    Backport stuff from Ubuntu

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -2,5 +2,5 @@
 
 pass show devel/ansible-vault/personal | \
     ansible-playbook --vault-password-file /bin/cat -i inventory \
-    -l "$(hostname)*" playbook.yml
+    -l "$(hostname)*" "$@" playbook.yml
 
