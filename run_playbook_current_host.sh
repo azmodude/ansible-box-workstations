e97fcbc0d1b9ad2d2b9970270b90c7d9798a48c6
Gordon Schulz <gordon.schulz@gmail.com>
2020-03-25 18:51:00 +0100

    Update for latest Ubuntu 20.04

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ! hash pass 2>/dev/null && sudo apt -y install pass
-#~/.venv/ansible/bin/ansible-galaxy install -r requirements.yml && \
+~/.venv/ansible/bin/ansible-galaxy install -r requirements.yml && \
 pass show devel/ansible-vault/personal | \
     ~/.venv/ansible/bin/ansible-playbook --vault-password-file /bin/cat -i inventory \
     -l "$(hostname)*" "$@" playbook.yml
