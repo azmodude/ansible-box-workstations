ecca4a5ddd6d2034a62c6a947817c71af2ea171a
Gordon Schulz <gordon.schulz@gmail.com>
2020-04-15 18:17:48 +0200

    Reformat file

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pass show devel/ansible-vault/personal | \
+pass show devel/ansible-vault/personal |
     ansible-playbook --vault-password-file /bin/cat -i inventory \
-    -l "$(hostname)*" "$@" playbook.yml
-
+        -l "$(hostname)*" "$@" playbook.yml
