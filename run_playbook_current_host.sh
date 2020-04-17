027f78046dd3f83debb5dbca4e9f593f3500614e
Gordon Schulz <gordon.schulz@gmail.com>
2020-04-17 21:02:13 +0200

    Add requirements install

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ansible-galaxy install -r requirements.yml && \
 pass show devel/ansible-vault/personal |
     ansible-playbook --vault-password-file /bin/cat -i inventory \
         -l "$(hostname)*" "$@" playbook.yml
