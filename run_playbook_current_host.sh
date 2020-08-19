6981449ff69768bb58129fd683836d020e278740
Gordon Schulz <gordon.schulz@gmail.com>
2020-08-19 12:12:55 +0200

    Adjust password location

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ansible-galaxy install -r requirements.yml && \
-pass show devel/ansible-vault/personal |
+pass show development/ansible-vault/personal |
     ansible-playbook --vault-password-file /bin/cat -i inventory \
         -l "$(hostname)*" "$@" playbook.yml
