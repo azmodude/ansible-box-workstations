9bcb4470c5ef870c4607b1f2b9e420e79e9a2980
Gordon Schulz <gordon.schulz@gmail.com>
2020-10-08 23:39:14 +0200

    Switch to secret-tool

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ansible-galaxy install -r requirements.yml && \
-pass show development/ansible-vault/personal |
+ansible-galaxy install -r requirements.yml &&
+    secret-tool lookup ansible-vault personal |
     ansible-playbook --vault-password-file /bin/cat -i inventory \
         -l "$(hostname)*" "$@" playbook.yml
