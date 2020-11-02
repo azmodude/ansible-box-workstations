b44aef59c24228619edda1fa80b6e79a62976aa0
Gordon Schulz <gordon.schulz@gmail.com>
2020-11-02 21:58:43 +0100

    Rename pass location

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -3,7 +3,7 @@
 ! hash pass 2>/dev/null && sudo apt -y install pass
 "${WORKON_HOME}/ansible/bin/ansible-galaxy" install -r requirements.yml && \
 #secret-tool lookup ansible-vault personal |
-pass show devel/ansible-vault/personal | \
+pass show development/ansible-vault/personal | \
     "${WORKON_HOME}/ansible/bin/ansible-playbook" \
     --vault-password-file /bin/cat -i inventory \
     -l "$(hostname)*" "$@" playbook.yml
