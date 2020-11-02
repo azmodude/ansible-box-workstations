bae9035e4dae497a1204f1cde5798cb24a5d73d5
Gordon Schulz <gordon.schulz@gmail.com>
2020-11-02 21:39:49 +0100

    libsecret -> pass

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ansible-galaxy install -r requirements.yml &&
-    secret-tool lookup ansible-vault personal |
+    pass show development/ansible-vault/personal pass show | \
     ansible-playbook --vault-password-file /bin/cat -i inventory \
         -l "$(hostname)*" "$@" playbook.yml
