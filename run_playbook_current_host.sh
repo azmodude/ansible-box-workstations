7d25572df6fd706a0d0af6a785da705389e8c22f
Gordon Schulz <gordon@gordonschulz.de>
2020-12-05 19:00:13 +0100

    Update to new install syntax

--- run_playbook_current_host.sh
+++ run_playbook_current_host.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ansible-galaxy install -r requirements.yml &&
-    pass show development/ansible-vault/personal pass show | \
+ansible-galaxy collection install -r requirements.yml &&
+    ansible-galaxy role install -r requirements.yml &&
+    pass show development/ansible-vault/personal pass show |
     ansible-playbook --vault-password-file /bin/cat -i inventory \
         -l "$(hostname)*" "$@" playbook.yml
